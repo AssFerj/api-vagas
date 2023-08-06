801df5bc2a2fb9694bebfbb2b222cb157954173c
AssFerj <assisjuniorcam@gmail.com>
2023-08-06 19:11:54 -0300

    feat: created getters from user model

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -13,4 +13,38 @@ export class User {
     ) {
         this._id = v4();
     }
+
+    public get id(): string {
+        return this._id;
+    }
+
+    public get name(): string {
+        return this._name;
+    }
+
+    public get email(): string {
+        return this._email;
+    }
+
+    public get password(): string {
+        return this._password;
+    }
+
+    public get enterpriseName(): string | undefined {
+        return this._enterpriseName;
+    }
+
+    public get type(): UserType {
+        return this._type;
+    }
+
+    public toJson() {
+        return {
+            id: this._id,
+            name: this._name,
+            email: this._email,
+            type: this._type,
+            enterpriseName: this._enterpriseName,
+        };
+    }
 }
