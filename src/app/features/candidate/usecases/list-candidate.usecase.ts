e6ad70fe52c375c5e920c29e918049bf373cc154
AssFerj <assisjuniorcam@gmail.com>
2023-08-12 18:41:03 -0300

    feat: add cache to list candidade usecase

--- src/app/features/candidate/usecases/list-candidate.usecase.ts
+++ src/app/features/candidate/usecases/list-candidate.usecase.ts
@@ -1,4 +1,5 @@
 import { UserType } from "../../../models/user-type.model";
+import { CacheRepository } from "../../../shared/database/repository/cache.repository";
 import { Result } from "../../../shared/util";
 import { Usecase } from "../../../shared/util/usecase.contract";
 import { UserRepository } from "../../user/repositories/user.repository";
@@ -6,12 +7,28 @@ import { UserRepository } from "../../user/repositories/user.repository";
 export class ListCandidateUsecase implements Usecase {
   public async execute(): Promise<Result> {
     const repository = new UserRepository();
+    const cache = new CacheRepository();
+
+    const cacheResult = await cache.get('list-candidate');
+
+    if(cacheResult) {
+      return {
+        ok: true,
+        message: "Candidates successfully listed in cache",
+        data: cacheResult,
+        code: 200,
+      };
+    }
+
     const result = await repository.list(UserType.Candidate);
+    const data = result?.map((candidate) => candidate.toJson());
+
+    await cache.set('list-candidate', data);
 
     return {
       ok: true,
       message: "Candidates successfully listed",
-      data: result?.map((candidate) => candidate.toJson()),
+      data: data,
       code: 200,
     };
   }
