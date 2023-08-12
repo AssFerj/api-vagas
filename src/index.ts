4c4e472d2a92a5036f711ccae6082cd899ecfd23
AssFerj <assisjuniorcam@gmail.com>
2023-08-12 17:25:38 -0300

    feat: installed redis and creat redis config

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import 'reflect-metadata';
 import { Database } from './main/database/database.connection';
 import { Server } from './main/server/express.server';
+import { RedisDatabase } from './main/database/redis.connection';
 
-Database.connect().then(()=>{
-    console.log('Database is connected');
+// Database.connect().then(()=>{
+//     console.log('Database is connected');
+//     Server.listen();
+// })
+
+Promise.all([Database.connect(), RedisDatabase.connect()]).then(() => {  
     Server.listen();
 })
---
+++ src/main/config/redis.config.ts
@@ -0,0 +1,11 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+// export default {
+//     host: process.env.REDIS_HOST!,
+//     port: Number(process.env.REDIS_PORT!),
+//     username: process.env.REDIS_USERNAME!,
+//     password: process.env.REDIS_PASSWORD!,
+// };
+
+export default process.env.REDIS_URL!;
