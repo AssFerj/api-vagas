38a0faf8cd8dfd0135bc8c0965080673484c1441
AssFerj <assisjuniorcam@gmail.com>
2023-08-12 18:41:28 -0300

    feat: add cache to list job application usecase

--- src/app/features/job-application/usecases/list-job-application.usecase.ts
+++ src/app/features/job-application/usecases/list-job-application.usecase.ts
@@ -1,16 +1,33 @@
+import { CacheRepository } from "../../../shared/database/repository/cache.repository";
 import { Usecase, Result } from "../../../shared/util";
 import { JobApplicationRepository } from "../repositories/job-application.repository";
 
 export class ListJobsApplication implements Usecase {
   public async execute(idCandidate: string): Promise<Result> {
     const repository = new JobApplicationRepository();
+    const cache = new CacheRepository();
+
+    const cacheResult = cache.get('list-job-application');
+
+    if(cacheResult) {
+      return {
+        ok: true,
+        message: "Job Applications successfully listed in cache",
+        data: cacheResult,
+        code: 200,
+      };
+    }
+
     const result = await repository.listByCandidateId(idCandidate);
+    const data = result?.map((job) => job?.job.toJson());
+
+    await cache.set('list-job-application', data);
 
     return {
       ok: true,
       code: 200,
       message: "Jobs application successfully listed",
-      data: result?.map((job) => job?.job.toJson()),
+      data: data,
     };
   }
 }
